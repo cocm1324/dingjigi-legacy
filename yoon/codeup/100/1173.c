1b08103af1edba23f21791dad4fac501bf932370
jaewonYoon <21300492@handong.edu>
2018-07-08 17:33:59 +0900

    Create 1173.c
    codeup 문제 중 맞춘 비율이 40%가 되지 않은 문제를 품

---
+++ yoon/codeup/100/1173.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+int input1=0;
+int input2=0;
+int minus_left = 30;
+int output1 = 0;
+int output2 = 0; 
+scanf("%d %d", &input1, &input2);
+if(input2 <30){
+minus_left= 30 - input2; 
+output2 = 60 - minus_left;
+	if(input1==0){
+	output1 = 23;
+	}
+	else output1= input1-1;
+}
+else{
+output2= input2-30;
+output1 =input1;
+
+}
+printf("%d %d\n", output1, output2); 
+return 0; 
+}
