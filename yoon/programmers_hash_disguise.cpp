cc6498e5d88525c09c1c651fc318d5705075b40b
jaewonYoon <21300492@handong.edu>
2018-12-20 09:38:11 +0900

    Update programmers_hash_disguise.cpp

--- yoon/programmers_hash_disguise.cpp
+++ yoon/programmers_hash_disguise.cpp
@@ -1,21 +1,23 @@
 #include <string>
 #include <vector>
-
+#include <map>
+#include <algorithm>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 0;
-    int size = clothes.size();
-    vector<int> num_clothes;
-    for(int i = 0 ; i < size; ++i){
-         num_clothes.push_back(clothes[i].size());
-    }
-    for(int i = 0;  i< num_clothes.size(); ++i){
-        if( i ==0) answer = num_clothes[i]+1;
-        else{
-            answer*= num_clothes[i]+1;
+    map<string,int> closet; 
+    map<string,int>::iterator iter;
+    int answer = 1;
+    int size_clothes = clothes.size();
+    for( int i = 0 ; i <size_clothes; ++i){
+        if(closet.find(clothes[i][1])==closet.end()){
+            closet.insert(make_pair(clothes[i][1],1));
         }
+        else closet.find(clothes[i][1])->second++;
     }
-    answer -=1; 
+    for( iter=closet.begin(); iter!=closet.end(); iter++){
+        answer *= (iter->second)+1;
+    }
+    answer -= 1;
     return answer;
 }
